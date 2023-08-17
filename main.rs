9f25a8ad2e4e74cf3b49c68557ff9b23715f501f
Jarrett Ye <jarrett.ye@outlook.com>
2023-08-17 12:16:41 +0800

    port step & forward

--- main.rs
+++ main.rs
@@ -1,149 +1,72 @@
 use burn::{
     module::{Module,
     Param,},
-    nn::{loss::CrossEntropyLoss},
     tensor::{
-        backend::{ADBackend, Backend},
-        Tensor,
+        backend::Backend,
+        Tensor, Float
     },
-    train::{ClassificationOutput, TrainOutput, TrainStep, ValidStep},
 };
 
 #[derive(Module, Debug)]
 pub struct Model<B: Backend> {
-    w0: Param<Tensor<B, 1>>,
-    w1: Param<Tensor<B, 1>>,
-    w2: Param<Tensor<B, 1>>,
-    w3: Param<Tensor<B, 1>>,
-    w4: Param<Tensor<B, 1>>,
-    w5: Param<Tensor<B, 1>>,
-    w6: Param<Tensor<B, 1>>,
-    w7: Param<Tensor<B, 1>>,
-    w8: Param<Tensor<B, 1>>,
-    w9: Param<Tensor<B, 1>>,
-    w10: Param<Tensor<B, 1>>,
-    w11: Param<Tensor<B, 1>>,
-    w12: Param<Tensor<B, 1>>,
-    w13: Param<Tensor<B, 1>>,
-    w14: Param<Tensor<B, 1>>,
-    w15: Param<Tensor<B, 1>>,
-    w16: Param<Tensor<B, 1>>,
+    w: Param<Tensor<B, 1>>,
 }
 
 impl<B: Backend> Model<B> {
     pub fn new() -> Self {
         // [0.4, 0.6, 2.4, 5.8, 4.93, 0.94, 0.86, 0.01, 1.49, 0.14, 0.94, 2.18, 0.05, 0.34, 1.26, 0.29, 2.61]
-        let w0 = Param::from(Tensor::from_floats([0.4]));
-        let w1 = Param::from(Tensor::from_floats([0.6]));
-        let w2 = Param::from(Tensor::from_floats([2.4]));
-        let w3 = Param::from(Tensor::from_floats([5.8]));
-        let w4 = Param::from(Tensor::from_floats([4.93]));
-        let w5 = Param::from(Tensor::from_floats([0.94]));
-        let w6 = Param::from(Tensor::from_floats([0.86]));
-        let w7 = Param::from(Tensor::from_floats([0.01]));
-        let w8 = Param::from(Tensor::from_floats([1.49]));
-        let w9 = Param::from(Tensor::from_floats([0.14]));
-        let w10 = Param::from(Tensor::from_floats([0.94]));
-        let w11 = Param::from(Tensor::from_floats([2.18]));
-        let w12 = Param::from(Tensor::from_floats([0.05]));
-        let w13 = Param::from(Tensor::from_floats([0.34]));
-        let w14 = Param::from(Tensor::from_floats([1.26]));
-        let w15 = Param::from(Tensor::from_floats([0.29]));
-        let w16 = Param::from(Tensor::from_floats([2.61]));
+        let w: Param<Tensor<B, 1>> = Param::from(Tensor::from_floats([0.4, 0.6, 2.4, 5.8, 4.93, 0.94, 0.86, 0.01, 1.49, 0.14, 0.94, 2.18, 0.05, 0.34, 1.26, 0.29, 2.61]));
         Self {
-            w0,
-            w1,
-            w2,
-            w3,
-            w4,
-            w5,
-            w6,
-            w7,
-            w8,
-            w9,
-            w10,
-            w11,
-            w12,
-            w13,
-            w14,
-            w15,
-            w16,
+            w
         }
     }
 
+    fn power_forgetting_curve(&self, t: Tensor<B, 1>, s: Tensor<B, 1>) -> Tensor<B, 1> {
+        let retrievability = (Tensor::ones_like(&t) + t / (self.w.slice([0..1]) * s * 9)).powf(-1.0);
+        retrievability
+    }
+
     fn stability_after_success(&self, last_s: Tensor<B, 1>, new_d: Tensor<B, 1>, r: Tensor<B, 1>, rating: Tensor<B, 1>) -> Tensor<B, 1> {
-        let hard_penalty = Tensor::ones_like(&rating).mask_where(rating.equal_elem(2), self.w15.val());
-        let easy_bonus = Tensor::ones_like(&rating).mask_where(rating.equal_elem(4), self.w16.val());
-        let new_s = last_s * (self.w8.val().exp() *
+        let hard_penalty = Tensor::ones_like(&rating).mask_where(rating.equal_elem(2), self.w.slice([15..16]));
+        let easy_bonus = Tensor::ones_like(&rating).mask_where(rating.equal_elem(4), self.w.slice([16..17]));
+        let new_s = last_s * (self.w.slice([8..9]).exp() *
             (-new_d + 11) *
-            (-self.w9.val() * last_s.log()).exp() *
-            ((r * -self.w10.val()).exp() - 1) * 
+            (-self.w.slice([9..10]) * last_s.log()).exp() *
+            ((r * -self.w.slice([10..11])).exp() - 1) * 
             hard_penalty * 
             easy_bonus + 1);
         new_s
     }
 
     fn stability_after_failure(&self, last_s: Tensor<B, 1>, new_d: Tensor<B, 1>, r: Tensor<B, 1>) -> Tensor<B, 1> {
-        let new_s = self.w11.val() *
-            (-self.w12.val() * new_d.log()).exp() *
-            ((self.w13.val() * (last_s + 1).log()).exp() - 1) *
-            ((-r + 1) * self.w14.val()).exp();
+        let new_s = self.w.slice([11..12]) *
+            (-self.w.slice([12..13]) * new_d.log()).exp() *
+            ((self.w.slice([13..14]) * (last_s + 1).log()).exp() - 1) *
+            ((-r + 1) * self.w.slice([14..15])).exp();
         new_s
     }
 
-    fn step(&self, i: i32, input: Tensor<B, 2>, state: Tensor<B, 2>) -> Tensor<B, 2> {
+    fn step(&self, i: i32, delta_t: Tensor<B, 1>, rating: Tensor<B, 1>, stability: Tensor<B, 1>, difficulty: Tensor<B, 1>) -> (Tensor<B, 1>, Tensor<B, 1>) {
         if i == 0 {
-            let keys = Tensor::from_ints([1, 2, 3, 4]).repeat(1, input.shape()[0]);
-            let index = 
+            let new_s = self.w.select(0, rating.int()); // TODO: int
+            let new_d = self.w.slice([4..5]) - self.w.slice([5..6]) * rating;
+            (new_s, new_d)
         } else {
-            
+            let r = self.power_forgetting_curve(delta_t, stability);
+            let new_d = difficulty + self.w.slice([1..2]) * (rating - self.w.slice([2..3]) * difficulty);
+            let new_d = new_d.clip(1, 10); // TODO: clip
+            let new_s = self.stability_after_success(stability, new_d, r, rating).mask_where(rating.equal_elem(1), self.stability_after_failure(stability, new_d, r));
+            (new_s, new_d)
         }
     }
 
-
-    pub fn forward(&self, input: Tensor<B, 3>) -> Tensor<B, 2> {
-        let [batch_size, heigth, width] = input.dims();
-
-        let x = input.reshape([batch_size, 1, heigth, width]).detach();
-        let x = self.conv1.forward(x);
-        let x = self.conv2.forward(x);
-        let x = self.conv3.forward(x);
-
-        let [batch_size, channels, heigth, width] = x.dims();
-        let x = x.reshape([batch_size, channels * heigth * width]);
-
-        let x = self.dropout.forward(x);
-        let x = self.fc1.forward(x);
-        let x = self.activation.forward(x);
-
-        self.fc2.forward(x)
-    }
-
-    pub fn forward_classification(&self, item: MNISTBatch<B>) -> ClassificationOutput<B> {
-        let targets = item.targets;
-        let output = self.forward(item.images);
-        let loss = CrossEntropyLoss::new(None);
-        let loss = loss.forward(output.clone(), targets.clone());
-
-        ClassificationOutput {
-            loss,
-            output,
-            targets,
+    pub fn forward(&self, delta_ts: Tensor<B, 2>, ratings: Tensor<B, 2, Float>, stabilitys: Tensor<B, 2>, difficultys: Tensor<B, 2>) -> (Tensor<B, 1>, Tensor<B, 1>) {
+        let [seq_len, batch_size] = delta_ts.dims();
+        let mut stability = Tensor::zeros([batch_size]);
+        let mut difficulty = Tensor::zeros([batch_size]);
+        for (i, delta_t, rating, stability, diffculty) in ().enumerate() { // TODO: enumerate
+            let (stability, difficulty) = self.step(i, delta_t, rating, stability, difficulty);
         }
-    }
-}
-
-
-impl<B: ADBackend> TrainStep<MNISTBatch<B>, ClassificationOutput<B>> for Model<B> {
-    fn step(&self, item: MNISTBatch<B>) -> TrainOutput<ClassificationOutput<B>> {
-        let item = self.forward_classification(item);
-
-        TrainOutput::new(self, item.loss.backward(), item)
-    }
-}
-
-impl<B: Backend> ValidStep<MNISTBatch<B>, ClassificationOutput<B>> for Model<B> {
-    fn step(&self, item: MNISTBatch<B>) -> ClassificationOutput<B> {
-        self.forward_classification(item)
+        (stability, difficulty)
     }
 }
