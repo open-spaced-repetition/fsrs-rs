0d67a5210147efeb206ade91716deda831d3dd10
Jarrett Ye <jarrett.ye@outlook.com>
2023-08-18 12:11:44 +0800

    fix clamp

--- src/model.rs
+++ src/model.rs
@@ -9,7 +9,7 @@ pub struct Model<B: Backend> {
     w: Param<Tensor<B, 1>>,
 }
 
-impl<B: Backend> Model<B> {
+impl<B: Backend<FloatElem = f32>> Model<B> {
     pub fn new() -> Self {
         Self {
             w: Param::from(Tensor::from_floats([
@@ -20,6 +20,10 @@ impl<B: Backend> Model<B> {
                 0.29, 2.61, // hard penalty, easy bonus
             ])),
         }
+    }
+
+    fn w(&self) -> Tensor<B, 1> {
+        self.w.val()
     }
 
     pub fn power_forgetting_curve(&self, t: Tensor<B, 1>, s: Tensor<B, 1>) -> Tensor<B, 1> {
@@ -36,14 +40,14 @@ impl<B: Backend> Model<B> {
     ) -> Tensor<B, 1> {
         let batch_size = rating.dims()[0];
         let hard_penalty = Tensor::ones([batch_size])
-            .mask_where(rating.clone().equal_elem(2), self.w.val().slice([15..16]));
+            .mask_where(rating.clone().equal_elem(2), self.w().slice([15..16]));
         let easy_bonus = Tensor::ones([batch_size])
-            .mask_where(rating.clone().equal_elem(4), self.w.val().slice([16..17]));
+            .mask_where(rating.clone().equal_elem(4), self.w().slice([16..17]));
         let new_s = last_s.clone()
-            * (self.w.val().slice([8..9]).exp()
+            * (self.w().slice([8..9]).exp()
                 * (-new_d + 11)
-                * (-self.w.val().slice([9..10]) * last_s.log()).exp()
-                * (((-r + 1) * self.w.val().slice([10..11])).exp() - 1)
+                * (-self.w().slice([9..10]) * last_s.log()).exp()
+                * (((-r + 1) * self.w().slice([10..11])).exp() - 1)
                 * hard_penalty
                 * easy_bonus
                 + 1);
@@ -56,10 +60,10 @@ impl<B: Backend> Model<B> {
         new_d: Tensor<B, 1>,
         r: Tensor<B, 1>,
     ) -> Tensor<B, 1> {
-        let new_s = self.w.val().slice([11..12])
-            * (-self.w.val().slice([12..13]) * new_d.log()).exp()
-            * ((self.w.val().slice([13..14]) * (last_s + 1).log()).exp() - 1)
-            * ((-r + 1) * self.w.val().slice([14..15])).exp();
+        let new_s = self.w().slice([11..12])
+            * (-self.w().slice([12..13]) * new_d.log()).exp()
+            * ((self.w().slice([13..14]) * (last_s + 1).log()).exp() - 1)
+            * ((-r + 1) * self.w().slice([14..15])).exp();
         new_s
     }
 
@@ -72,15 +76,14 @@ impl<B: Backend> Model<B> {
         difficulty: Tensor<B, 1>,
     ) -> (Tensor<B, 1>, Tensor<B, 1>) {
         if i == 0 {
-            let new_s = self.w.val().select(0, rating.clone().int() - 1);
-            let new_d = self.w.val().slice([4..5]) - self.w.val().slice([5..6]) * (rating - 3);
+            let new_s = self.w().select(0, rating.clone().int() - 1);
+            let new_d = self.w().slice([4..5]) - self.w().slice([5..6]) * (rating - 3);
             (new_s, new_d)
         } else {
             let r = self.power_forgetting_curve(delta_t, stability.clone());
             dbg!(&r);
-            let new_d = difficulty.clone() - self.w.val().slice([6..7]) * (rating.clone() - 3);
-            // let new_d = new_d.clamp(1.0, 10.0);
-            // TODO: consider constraining the associated type `<B as Backend>::FloatElem` to `{float}` or calling a method that returns `<B as Backend>::FloatElem`
+            let new_d = difficulty.clone() - self.w().slice([6..7]) * (rating.clone() - 3);
+            let new_d = new_d.clamp(1.0, 10.0);
             dbg!(&new_d);
             let s_recall = self.stability_after_success(
                 stability.clone(),
@@ -122,7 +125,7 @@ pub struct ModelConfig {
 }
 
 impl ModelConfig {
-    pub fn init<B: Backend>(&self) -> Model<B> {
+    pub fn init<B: Backend<FloatElem = f32>>(&self) -> Model<B> {
         Model::new()
     }
 }
