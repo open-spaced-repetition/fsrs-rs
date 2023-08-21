1b26d04022cb238cf7594b68f922f7bc18d219c1
Luc Mcgrady <lucmcgrady@gmail.com>
2023-08-21 22:52:54 +0100

    Fix: no deep array

--- src/python_bindings.rs
+++ src/python_bindings.rs
@@ -6,53 +6,51 @@ use crate::{
 use burn::optim::AdamConfig;
 use log::debug;
 use pyo3::{types::*, *};
+use std::format;
 
 #[pyfunction]
-fn py_train(py: Python, revlogs: &PyList) -> PyResult<()> {
+fn py_train(_py: Python, revlogs: &PyList) -> PyResult<()> {
     use burn_ndarray::NdArrayBackend;
     use burn_ndarray::NdArrayDevice;
     type Backend = NdArrayBackend<f32>;
     type AutodiffBackend = burn_autodiff::ADBackendDecorator<Backend>;
 
-    let revlog: Vec<Vec<RevlogEntry>> = revlogs
+    let revlog: Vec<RevlogEntry> = revlogs
         .into_iter()
         .map(|e| {
-            let list: &PyList = pyo3::PyTryInto::try_into(e).unwrap();
+            let obj: &PyDict = pyo3::PyTryInto::try_into(e).unwrap();
 
-            list.into_iter()
-                .map(|e2| {
-                    macro_rules! get {
-                        ($key:expr) => {
-                            e2.get_item($key)
-                                .expect("Missing attr")
-                                .extract()
-                                .expect("Wrong type")
-                        };
-                    }
+            macro_rules! get {
+                ($key:expr) => {
+                    obj.get_item($key)
+                        .expect(&format!("Missing attr: {}", $key))
+                        .extract()
+                        .expect(&format!("Wrong type for key: {}", $key))
+                };
+            }
 
-                    RevlogEntry {
-                        id: get!("id"),
-                        cid: get!("cid"),
-                        usn: get!("usn"),
-                        button_chosen: get!("button_chosen"),
-                        interval: get!("interval"),
-                        last_interval: get!("last_interval"),
-                        ease_factor: get!("ease_factor"),
-                        taken_millis: get!("taken_millis"),
-                        review_kind: get!("review_kind"),
-                        delta_t: get!("delta_t"),
-                        i: get!("i"),
-                        r_history: get!("r_history"),
-                        t_history: get!("t_history"),
-                    }
-                })
-                .collect::<Vec<RevlogEntry>>() // https://regex101.com/r/sVxrTW/1
+            RevlogEntry {
+                id: get!("id"),
+                cid: get!("cid"),
+                usn: get!("usn"),
+                button_chosen: get!("button_chosen"),
+                interval: get!("interval"),
+                last_interval: get!("last_interval"),
+                ease_factor: get!("ease_factor"),
+                taken_millis: get!("taken_millis"),
+                review_kind: get!("review_kind"),
+                delta_t: get!("delta_t"),
+                i: get!("i"),
+                r_history: get!("r_history"),
+                t_history: get!("t_history"),
+            }
+
         })
         .collect();
 
     debug!("revlogs: {:?}", revlog);
 
-    let items = convert_to_fsrs_items(revlog);
+    let items = convert_to_fsrs_items(vec![revlog]);
 
     let result = train::<AutodiffBackend>(
         "./tmp/fsrs",
@@ -61,6 +59,7 @@ fn py_train(py: Python, revlogs: &PyList) -> PyResult<()> {
         &items,
     );
 
+    println!("{:?}", result);
     //let list = PyList::empty(py);
     //list.append(item)
     //let list = PyList::new(py, result);
