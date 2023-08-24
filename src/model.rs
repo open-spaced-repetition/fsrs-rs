22d56ef007573c9f020fe75f17c275701d11a89b
Jarrett Ye <jarrett.ye@outlook.com>
2023-08-25 00:58:19 +0800

    add reshape for slice of w

--- src/model.rs
+++ src/model.rs
@@ -41,16 +41,16 @@ impl<B: Backend<FloatElem = f32>> Model<B> {
         let batch_size = rating.dims()[0];
         let hard_penalty = Tensor::ones([batch_size, 1]).mask_where(
             rating.clone().equal_elem(2),
-            self.w().slice([15..16]).unsqueeze(),
+            self.w().slice([15..16]).reshape([1, 1]).unsqueeze(),
         );
         let easy_bonus = Tensor::ones([batch_size, 1])
-            .mask_where(rating.equal_elem(4), self.w().slice([16..17]).unsqueeze());
+            .mask_where(rating.equal_elem(4), self.w().slice([16..17]).reshape([1, 1]).unsqueeze());
 
         last_s.clone()
-            * (self.w().slice([8..9]).exp()
+            * (self.w().slice([8..9]).reshape([1, 1]).exp()
                 * (-new_d + 11)
-                * (-self.w().slice([9..10]) * last_s.log()).exp()
-                * (((-r + 1) * self.w().slice([10..11])).exp() - 1)
+                * (-self.w().slice([9..10]).reshape([1, 1]) * last_s.log()).exp()
+                * (((-r + 1) * self.w().slice([10..11])).reshape([1, 1]).exp() - 1)
                 * hard_penalty
                 * easy_bonus
                 + 1)
@@ -62,14 +62,14 @@ impl<B: Backend<FloatElem = f32>> Model<B> {
         new_d: Tensor<B, 2>,
         r: Tensor<B, 2>,
     ) -> Tensor<B, 2> {
-        self.w().slice([11..12])
-            * (-self.w().slice([12..13]) * new_d.log()).exp()
-            * ((self.w().slice([13..14]) * (last_s + 1).log()).exp() - 1)
-            * ((-r + 1) * self.w().slice([14..15])).exp()
+        self.w().slice([11..12]).reshape([1, 1])
+            * (-self.w().slice([12..13]).reshape([1, 1]) * new_d.log()).exp()
+            * ((self.w().slice([13..14]).reshape([1, 1]) * (last_s + 1).log()).exp() - 1)
+            * ((-r + 1) * self.w().slice([14..15]).reshape([1, 1])).exp()
     }
 
     fn mean_reversion(&self, new_d: Tensor<B, 2>) -> Tensor<B, 2> {
-        self.w().slice([7..8]) * (self.w().slice([4..5]) - new_d.clone()) + new_d
+        self.w().slice([7..8]).reshape([1, 1]) * (self.w().slice([4..5]).reshape([1, 1]) - new_d.clone()) + new_d
     }
 
     fn init_stability(&self, rating: Tensor<B, 2>) -> Tensor<B, 2> {
@@ -81,11 +81,11 @@ impl<B: Backend<FloatElem = f32>> Model<B> {
     }
 
     fn init_difficulty(&self, rating: Tensor<B, 2>) -> Tensor<B, 2> {
-        self.w().slice([4..5]) - self.w().slice([5..6]) * (rating - 3)
+        self.w().slice([4..5]).reshape([1, 1]) - self.w().slice([5..6]).reshape([1, 1]) * (rating - 3)
     }
 
     fn next_difficulty(&self, difficulty: Tensor<B, 2>, rating: Tensor<B, 2>) -> Tensor<B, 2> {
-        difficulty - self.w().slice([6..7]) * (rating - 3)
+        difficulty - self.w().slice([6..7]).reshape([1, 1]) * (rating - 3)
     }
 
     fn step(
@@ -299,4 +299,62 @@ fn test_forward() {
     let (stability, difficulty) = model.forward(delta_ts, ratings);
     println!("stability {:?}", stability);
     println!("difficulty {:?}", difficulty);
+}
+
+#[cfg(test)]
+mod tests {
+    use burn::tensor::Data;
+    use burn_autodiff::ADBackendDecorator;
+    use burn_ndarray::NdArrayBackend;
+    type Backend = ADBackendDecorator<NdArrayBackend<f32>>;
+    use super::*;
+
+    #[test]
+    fn test_next_difficulty() {
+        let model = Model::<Backend>::new();
+        let difficulty = Tensor::<Backend, 2>::from_floats([[5.0], [5.0], [5.0], [5.0]]);
+        let rating = Tensor::<Backend, 2>::from_floats([[1.0], [2.0], [3.0], [4.0]]);
+        let next_difficulty = model.next_difficulty(difficulty, rating);
+        next_difficulty.clone().backward();
+        assert_eq!(
+            next_difficulty.to_data(),
+            Data::from([[6.7200003], [5.86], [5.0], [4.14]])
+        );
+        let next_difficulty = model.mean_reversion(next_difficulty);
+        next_difficulty.clone().backward();
+        assert_eq!(
+            next_difficulty.to_data(),
+            Data::from([[6.7021003], [5.8507], [4.9993], [4.1478996]])
+        )
+    }
+
+    #[test]
+    fn test_next_stability() {
+        let model = Model::<Backend>::new();
+        let stability = Tensor::<Backend, 2>::from_floats([[5.0], [5.0], [5.0], [5.0]]);
+        let difficulty = Tensor::<Backend, 2>::from_floats([[1.0], [2.0], [3.0], [4.0]]);
+        let retention = Tensor::<Backend, 2>::from_floats([[0.9], [0.8], [0.7], [0.6]]);
+        let rating = Tensor::<Backend, 2>::from_floats([[1.0], [2.0], [3.0], [4.0]]);
+        let s_recall = model.stability_after_success(
+            stability.clone(),
+            difficulty.clone(),
+            retention.clone(),
+            rating.clone(),
+        );
+        s_recall.clone().backward();
+        assert_eq!(
+            s_recall.to_data(),
+            Data::from([[22.454704], [14.560361], [51.15574], [152.6869]])
+        );
+        let s_forget = model.stability_after_failure(stability, difficulty, retention);
+        assert_eq!(
+            s_forget.to_data(),
+            Data::from([[2.074517], [2.2729328], [2.526406], [2.8247323]])
+        );
+        let next_stability = s_recall.mask_where(rating.clone().equal_elem(1), s_forget);
+        assert_eq!(
+            next_stability.to_data(),
+            Data::from([[2.074517], [14.560361], [51.15574], [152.6869]])
+        )
+    }
 }
