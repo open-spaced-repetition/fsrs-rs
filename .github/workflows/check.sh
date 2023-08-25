4ad67240e2959a33267ae7fedc9e357f28ec0699
AsukaMinato <i@asukaminato.eu.org>
2023-08-25 15:03:10 +0900

    add test into ci

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -9,3 +9,9 @@ cargo fmt --check || (
 )
 
 cargo clippy -- -Dwarnings
+
+install -d tests/data/
+pushd tests/data/
+wget https://github.com/open-spaced-repetition/fsrs-optimizer-burn/files/12394182/collection.anki21.zip
+unzip *.zip
+RUST_BACKTRACE=full cargo test --release # may occur test cosine_annealing::test_lr_scheduler ... FAILED
