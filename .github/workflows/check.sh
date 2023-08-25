2d025019b660a4b080c59b521b729148f9ad4957
AsukaMinato <i@asukaminato.eu.org>
2023-08-25 16:09:17 +0900

    add wget for windows

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -12,6 +12,8 @@ cargo clippy -- -Dwarnings
 
 install -d tests/data/
 pushd tests/data/
+
+[ $(uname -s) == MSYS_NT* ] && choco install wget
 wget https://github.com/open-spaced-repetition/fsrs-optimizer-burn/files/12394182/collection.anki21.zip
 unzip *.zip
 RUST_BACKTRACE=full cargo test --release # very slow, need cache
