bbb78dcaa6584bf14159921d75c1ae55e4a8d5a0
AsukaMinato <i@asukaminato.eu.org>
2023-08-31 02:58:56 +0900

    some simplify, use assert_eq.

--- src/pre_training.rs
+++ src/pre_training.rs
@@ -114,7 +114,7 @@ fn insert_default_point(
 }
 
 fn search_parameters(pretrainset: HashMap<i32, HashMap<String, Vec<f32>>>) -> HashMap<i32, f32> {
-    let mut optimal_stabilities: HashMap<i32, f32> = HashMap::new();
+    let mut optimal_stabilities = HashMap::new();
     let epsilon = f32::EPSILON;
 
     for (first_rating, data) in pretrainset.iter() {
@@ -200,7 +200,7 @@ fn smooth_and_fill(
                 rating_stability.contains_key(&3),
                 rating_stability.contains_key(&4),
             ) {
-                (false, false, true, true) => {
+                (false, false, _, _) => {
                     rating_stability.insert(
                         2,
                         rating_stability[&3].powf(1.0 / (1.0 - w2))
@@ -212,7 +212,7 @@ fn smooth_and_fill(
                             * f32::powf(rating_stability[&3], 1.0 - 1.0 / w1),
                     );
                 }
-                (false, true, false, true) => {
+                (false, _, false, _) => {
                     rating_stability.insert(
                         3,
                         f32::powf(rating_stability[&2], 1.0 - w2)
@@ -224,7 +224,7 @@ fn smooth_and_fill(
                             * f32::powf(rating_stability[&3], 1.0 - 1.0 / w1),
                     );
                 }
-                (false, true, true, false) => {
+                (false, _, _, false) => {
                     rating_stability.insert(
                         4,
                         rating_stability[&2].powf(1.0 - 1.0 / w2)
@@ -236,7 +236,7 @@ fn smooth_and_fill(
                             * rating_stability[&3].powf(1.0 - 1.0 / w1),
                     );
                 }
-                (true, false, false, true) => {
+                (_, false, false, _) => {
                     rating_stability.insert(
                         2,
                         rating_stability[&1].powf(w1 / (w1 + w2 - w1 * w2))
@@ -248,7 +248,7 @@ fn smooth_and_fill(
                             * rating_stability[&4].powf(w2 / (w1 + w2 - w1 * w2)),
                     );
                 }
-                (true, false, true, false) => {
+                (_, false, _, false) => {
                     rating_stability.insert(
                         2,
                         rating_stability[&1].powf(w1) * rating_stability[&3].powf(1.0 - w1),
@@ -259,7 +259,7 @@ fn smooth_and_fill(
                             * rating_stability[&3].powf(1.0 / w2),
                     );
                 }
-                (true, true, false, false) => {
+                (_, _, false, false) => {
                     rating_stability.insert(
                         3,
                         rating_stability[&1].powf(1.0 - 1.0 / (1.0 - w1))
@@ -273,9 +273,11 @@ fn smooth_and_fill(
                 }
                 _ => {}
             }
-            let mut sorted_items: Vec<_> = rating_stability.iter().collect();
-            sorted_items.sort_by(|a, b| a.0.cmp(b.0));
-            init_s0 = sorted_items.iter().map(|&(_, &value)| value).collect();
+            init_s0 = rating_stability
+                .iter()
+                .sorted_by(|a, b| a.0.cmp(b.0))
+                .map(|(_, &v)| v)
+                .collect();
         }
         3 => {
             match (
@@ -312,9 +314,11 @@ fn smooth_and_fill(
                 }
                 _ => {}
             }
-            let mut sorted_items: Vec<_> = rating_stability.iter().collect();
-            sorted_items.sort_by(|a, b| a.0.cmp(b.0));
-            init_s0 = sorted_items.iter().map(|&(_, &value)| value).collect();
+            init_s0 = rating_stability
+                .iter()
+                .sorted_by(|a, b| a.0.cmp(b.0))
+                .map(|(_, &v)| v)
+                .collect();
         }
         4 => {
             init_s0 = rating_stability
@@ -351,7 +355,7 @@ fn test_loss() {
 
 #[test]
 fn test_search_parameters() {
-    let pretrainset: HashMap<i32, HashMap<String, Vec<f32>>> = HashMap::from([(
+    let pretrainset = HashMap::from([(
         4,
         HashMap::from([
             ("delta_t".to_string(), vec![1.0, 2.0, 3.0, 4.0]),
@@ -360,7 +364,7 @@ fn test_search_parameters() {
         ]),
     )]);
     let actual = search_parameters(pretrainset);
-    let expected = [(4, 1.0714815)].iter().cloned().collect();
+    let expected = [(4, 1.0714815)].into_iter().collect();
     assert_eq!(actual, expected);
 }
 
@@ -378,5 +382,5 @@ fn test_smooth_and_fill() {
     let mut rating_stability = HashMap::from([(1, 0.4), (3, 2.4), (4, 5.8)]);
     let rating_count = HashMap::from([(1, 1), (2, 1), (3, 1), (4, 1)]);
     let actual = smooth_and_fill(&mut rating_stability, &rating_count);
-    dbg!(actual);
+    assert_eq!(actual, [0.4, 0.81906897, 2.4, 5.8,]);
 }
