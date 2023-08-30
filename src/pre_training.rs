ac15ec1461e1fd7ff08f3e65c3705f36ac6376b7
Jarrett Ye <jarrett.ye@outlook.com>
2023-08-30 11:43:36 +0800

    implement smooth_and_fill

--- src/pre_training.rs
+++ src/pre_training.rs
@@ -1,56 +1,9 @@
+use itertools::Itertools;
 use ndarray::Array1;
 use std::collections::HashMap;
+use std::iter::Iterator;
 
 use crate::FSRSItem;
-
-fn power_forgetting_curve(t: &Array1<f32>, s: f32) -> Array1<f32> {
-    1.0 / (1.0 + t / (9.0 * s))
-}
-
-fn loss(delta_t: &Array1<f32>, recall: &Array1<f32>, count: &Array1<f32>, init_s0: f32) -> f32 {
-    let y_pred = power_forgetting_curve(delta_t, init_s0);
-    let diff = recall - &y_pred;
-    let weighted_diff = &diff * &diff * count;
-    let rmse = f32::sqrt(weighted_diff.sum() / count.sum());
-    // TODO: add l1 regularization
-    rmse
-}
-
-fn search_parameters(pretrainset: HashMap<i32, HashMap<String, Vec<f32>>>) -> HashMap<i32, f32> {
-    let mut optimal_stabilities: HashMap<i32, f32> = HashMap::new();
-
-    let epsilon = 1e-6; // precision
-
-    for (first_rating, data) in pretrainset.iter() {
-        let delta_t = Array1::from(data["delta_t"].clone());
-        let recall = Array1::from(data["recall"].clone());
-        let count = Array1::from(data["count"].clone());
-
-        let mut low = 0.0;
-        let mut high = 100.0;
-        let mut optimal_s = 0.0;
-
-        while high - low > epsilon {
-            let mid1 = low + (high - low) / 3.0;
-            let mid2 = high - (high - low) / 3.0;
-
-            let loss1 = loss(&delta_t, &recall, &count, mid1);
-            let loss2 = loss(&delta_t, &recall, &count, mid2);
-
-            if loss1 < loss2 {
-                high = mid2;
-            } else {
-                low = mid1;
-            }
-
-            optimal_s = (high + low) / 2.0;
-        }
-
-        optimal_stabilities.insert(*first_rating, optimal_s);
-    }
-
-    optimal_stabilities
-}
 
 fn create_pretrain_data(fsrs_items: Vec<FSRSItem>) -> HashMap<i32, HashMap<String, Vec<f32>>> {
     // filter FSRSItem instances with exactly 2 reviews.
@@ -101,12 +54,209 @@ fn create_pretrain_data(fsrs_items: Vec<FSRSItem>) -> HashMap<i32, HashMap<Strin
     results
 }
 
-#[test]
-fn test_search_parameters() {
-    use crate::convertor::tests::anki21_sample_file_converted_to_fsrs;
-    let fsrs_items = anki21_sample_file_converted_to_fsrs();
-    let pre_train_data = create_pretrain_data(fsrs_items);
-    dbg!(search_parameters(pre_train_data));
+fn total_rating_count(pretrainset: HashMap<i32, HashMap<String, Vec<f32>>>) -> HashMap<i32, i32> {
+    let mut rating_count: HashMap<i32, i32> = HashMap::new();
+    for (first_rating, data) in pretrainset.iter() {
+        let count = data["count"].iter().sum::<f32>() as i32;
+        rating_count.insert(*first_rating, count);
+    }
+    rating_count
+}
+
+fn power_forgetting_curve(t: &Array1<f32>, s: f32) -> Array1<f32> {
+    1.0 / (1.0 + t / (9.0 * s))
+}
+
+fn loss(delta_t: &Array1<f32>, recall: &Array1<f32>, count: &Array1<f32>, init_s0: f32) -> f32 {
+    let y_pred = power_forgetting_curve(delta_t, init_s0);
+    let diff = recall - &y_pred;
+    let weighted_diff = &diff * &diff * count;
+    let rmse = f32::sqrt(weighted_diff.sum() / count.sum());
+    // TODO: add l1 regularization
+    rmse
+}
+
+fn search_parameters(pretrainset: HashMap<i32, HashMap<String, Vec<f32>>>) -> HashMap<i32, f32> {
+    let mut optimal_stabilities: HashMap<i32, f32> = HashMap::new();
+
+    let epsilon = 1e-6; // precision
+
+    for (first_rating, data) in pretrainset.iter() {
+        let delta_t = Array1::from(data["delta_t"].clone());
+        let recall = Array1::from(data["recall"].clone());
+        let count = Array1::from(data["count"].clone());
+
+        let mut low = 0.0;
+        let mut high = 100.0;
+        let mut optimal_s = 0.0;
+
+        while high - low > epsilon {
+            let mid1 = low + (high - low) / 3.0;
+            let mid2 = high - (high - low) / 3.0;
+
+            let loss1 = loss(&delta_t, &recall, &count, mid1);
+            let loss2 = loss(&delta_t, &recall, &count, mid2);
+
+            if loss1 < loss2 {
+                high = mid2;
+            } else {
+                low = mid1;
+            }
+
+            optimal_s = (high + low) / 2.0;
+        }
+
+        optimal_stabilities.insert(*first_rating, optimal_s);
+    }
+
+    optimal_stabilities
+}
+
+fn smooth_and_fill(
+    rating_stability: &mut HashMap<i32, f32>,
+    rating_count: &HashMap<i32, i32>,
+) -> Vec<f32> {
+    for &(small_rating, big_rating) in &[(1, 2), (2, 3), (3, 4), (1, 3), (2, 4), (1, 4)] {
+        if let (Some(&small_value), Some(&big_value)) = (
+            rating_stability.get(&small_rating),
+            rating_stability.get(&big_rating),
+        ) {
+            if small_value > big_value {
+                if rating_count[&small_rating] > rating_count[&big_rating] {
+                    rating_stability.insert(big_rating, small_value);
+                } else {
+                    rating_stability.insert(small_rating, big_value);
+                }
+            }
+        }
+    }
+
+    let w1 = 3.0 / 5.0;
+    let w2 = 3.0 / 5.0;
+
+    let mut init_s0 = vec![];
+
+    let r_s0_default = [(1, 0.4), (2, 0.6), (3, 2.4), (4, 5.8)]
+        .iter()
+        .cloned()
+        .collect::<HashMap<_, _>>();
+
+    match rating_stability.len() {
+        0 => panic!("Not enough data for pretraining!"),
+        1 => {
+            let rating = rating_stability.keys().next().unwrap();
+            let factor = rating_stability[rating] / r_s0_default[rating];
+            init_s0 = r_s0_default.values().map(|&x| x * factor).collect();
+        }
+        2 => {
+            if !rating_stability.contains_key(&1) && !rating_stability.contains_key(&2) {
+                rating_stability.insert(
+                    2,
+                    f32::powf(rating_stability[&3], 1.0 / (1.0 - w2))
+                        * f32::powf(rating_stability[&4], 1.0 - 1.0 / (1.0 - w2)),
+                );
+                rating_stability.insert(
+                    1,
+                    f32::powf(rating_stability[&2], 1.0 / w1)
+                        * f32::powf(rating_stability[&3], 1.0 - 1.0 / w1),
+                );
+            } else if !rating_stability.contains_key(&1) && !rating_stability.contains_key(&3) {
+                rating_stability.insert(
+                    3,
+                    f32::powf(rating_stability[&2], 1.0 - w2) * f32::powf(rating_stability[&4], w2),
+                );
+                rating_stability.insert(
+                    1,
+                    f32::powf(rating_stability[&2], 1.0 / w1)
+                        * f32::powf(rating_stability[&3], 1.0 - 1.0 / w1),
+                );
+            } else if !rating_stability.contains_key(&1) && !rating_stability.contains_key(&4) {
+                rating_stability.insert(
+                    4,
+                    f32::powf(rating_stability[&2], 1.0 - 1.0 / w2)
+                        * f32::powf(rating_stability[&3], 1.0 / w2),
+                );
+                rating_stability.insert(
+                    1,
+                    f32::powf(rating_stability[&2], 1.0 / w1)
+                        * f32::powf(rating_stability[&3], 1.0 - 1.0 / w1),
+                );
+            } else if !rating_stability.contains_key(&2) && !rating_stability.contains_key(&3) {
+                rating_stability.insert(
+                    2,
+                    f32::powf(rating_stability[&1], w1 / (w1 + w2 - w1 * w2))
+                        * f32::powf(rating_stability[&4], 1.0 - w1 / (w1 + w2 - w1 * w2)),
+                );
+                rating_stability.insert(
+                    3,
+                    f32::powf(rating_stability[&1], 1.0 - w2 / (w1 + w2 - w1 * w2))
+                        * f32::powf(rating_stability[&4], w2 / (w1 + w2 - w1 * w2)),
+                );
+            } else if !rating_stability.contains_key(&2) && !rating_stability.contains_key(&4) {
+                rating_stability.insert(
+                    2,
+                    f32::powf(rating_stability[&1], w1) * f32::powf(rating_stability[&3], 1.0 - w1),
+                );
+                rating_stability.insert(
+                    4,
+                    f32::powf(rating_stability[&2], 1.0 - 1.0 / w2)
+                        * f32::powf(rating_stability[&3], 1.0 / w2),
+                );
+            } else if !rating_stability.contains_key(&3) && !rating_stability.contains_key(&4) {
+                rating_stability.insert(
+                    3,
+                    f32::powf(rating_stability[&1], 1.0 - 1.0 / (1.0 - w1))
+                        * f32::powf(rating_stability[&2], 1.0 / (1.0 - w1)),
+                );
+                rating_stability.insert(
+                    4,
+                    f32::powf(rating_stability[&2], 1.0 - 1.0 / w2)
+                        * f32::powf(rating_stability[&3], 1.0 / w2),
+                );
+            }
+            let mut sorted_items: Vec<_> = rating_stability.iter().collect();
+            sorted_items.sort_by(|a, b| a.0.cmp(&b.0));
+            init_s0 = sorted_items.iter().map(|&(_, &value)| value).collect();
+        }
+        3 => {
+            if !rating_stability.contains_key(&1) {
+                rating_stability.insert(
+                    1,
+                    f32::powf(rating_stability[&2], 1.0 / w1)
+                        * f32::powf(rating_stability[&3], 1.0 - 1.0 / w1),
+                );
+            } else if !rating_stability.contains_key(&2) {
+                rating_stability.insert(
+                    2,
+                    f32::powf(rating_stability[&1], w1) * f32::powf(rating_stability[&3], 1.0 - w1),
+                );
+            } else if !rating_stability.contains_key(&3) {
+                rating_stability.insert(
+                    3,
+                    f32::powf(rating_stability[&2], 1.0 - w2) * f32::powf(rating_stability[&4], w2),
+                );
+            } else if !rating_stability.contains_key(&4) {
+                rating_stability.insert(
+                    4,
+                    f32::powf(rating_stability[&2], 1.0 - 1.0 / w2)
+                        * f32::powf(rating_stability[&3], 1.0 / w2),
+                );
+            }
+            let mut sorted_items: Vec<_> = rating_stability.iter().collect();
+            sorted_items.sort_by(|a, b| a.0.cmp(&b.0));
+            init_s0 = sorted_items.iter().map(|&(_, &value)| value).collect();
+        }
+        4 => {
+            init_s0 = rating_stability
+                .iter()
+                .sorted_by(|a, b| a.0.cmp(&b.0))
+                .map(|(_, &v)| v)
+                .collect();
+        }
+        _ => {}
+    }
+
+    init_s0
 }
 
 #[test]
@@ -128,4 +278,29 @@ fn test_loss() {
     let actual = loss(&delta_t, &recall, &count, init_s0);
     assert_eq!(actual, expected);
     assert_eq!(loss(&delta_t, &recall, &count, 2.0), 0.07144503);
+}
+
+#[test]
+fn test() {
+    use crate::convertor::tests::anki21_sample_file_converted_to_fsrs;
+    let fsrs_items = anki21_sample_file_converted_to_fsrs();
+    let pre_train_data = create_pretrain_data(fsrs_items);
+    let rating_count = total_rating_count(pre_train_data.clone());
+    let rating_stability = search_parameters(pre_train_data);
+    let init_s0 = smooth_and_fill(&mut rating_stability.clone(), &rating_count);
+    dbg!(init_s0);
+}
+
+#[test]
+fn test_smooth_and_fill() {
+    let mut rating_stability = HashMap::new();
+    rating_stability.insert(1, 0.4);
+    rating_stability.insert(3, 2.4);
+    rating_stability.insert(4, 5.8);
+    let rating_count = [(1, 1), (2, 1), (3, 1), (4, 1)]
+        .iter()
+        .cloned()
+        .collect::<HashMap<_, _>>();
+    let actual = smooth_and_fill(&mut rating_stability, &rating_count);
+    dbg!(actual);
 }
