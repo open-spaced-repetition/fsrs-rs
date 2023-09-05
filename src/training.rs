b1d77ede1cb569fb1757e85a9efe2dc2fc4701a4
Jarrett Ye <jarrett.ye@outlook.com>
2023-09-05 17:54:19 +0800

    increase num_epochs & decrease learning_rate

--- src/training.rs
+++ src/training.rs
@@ -177,7 +177,7 @@ impl DashboardRenderer for ProgressCollector {
 pub(crate) struct TrainingConfig {
     pub model: ModelConfig,
     pub optimizer: AdamConfig,
-    #[config(default = 10)]
+    #[config(default = 16)]
     pub num_epochs: usize,
     #[config(default = 512)]
     pub batch_size: usize,
@@ -185,7 +185,7 @@ pub(crate) struct TrainingConfig {
     pub num_workers: usize,
     #[config(default = 42)]
     pub seed: u64,
-    #[config(default = 8.0e-3)]
+    #[config(default = 1.0e-3)]
     pub learning_rate: f64,
 }
 
