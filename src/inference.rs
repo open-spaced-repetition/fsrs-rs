b591addb61a7302f15f8fd58a1d8e67df350cde0
Jarrett Ye <jarrett.ye@outlook.com>
2023-09-09 08:26:33 +0800

    Feat/support scheduler (#58)
    * Feat/support scheduler
    
    calc_memo_state, evaluate, next_interval, next_memo_state
    
    * use &[ ]
    
    ---------
    
    Co-authored-by: AsukaMinato <i@asukaminato.eu.org>

--- src/inference.rs
+++ src/inference.rs
@@ -13,7 +13,7 @@ use crate::dataset::FSRSBatcher;
 use crate::error::Result;
 use crate::model::Model;
 use crate::training::BCELoss;
-use crate::{FSRSError, FSRSItem};
+use crate::{FSRSError, FSRSItem, FSRSReview};
 
 fn infer<B: Backend<FloatElem = f32>>(
     model: &Model<B>,
@@ -27,25 +27,84 @@ fn infer<B: Backend<FloatElem = f32>>(
     (stability, difficulty, retention)
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct ItemProgress {
-    pub current: usize,
-    pub total: usize,
-}
-
-pub fn evaluate<F>(weights: [f32; 17], items: Vec<FSRSItem>, mut progress: F) -> Result<(f32, f32)>
-where
-    F: FnMut(ItemProgress) -> bool,
-{
+fn weights_to_modela(weights: &[f32; 17]) -> Model<NdArrayBackend<f32>> {
     type Backend = NdArrayBackend<f32>;
-    let device = NdArrayDevice::Cpu;
-    let batcher = FSRSBatcher::<Backend>::new(device);
     let config = ModelConfig::default();
     let mut model = Model::<Backend>::new(config);
     model.w = Param::from(Tensor::from_floats(Data::new(
         weights.to_vec(),
         Shape { dims: [17] },
     )));
+    model
+}
+
+pub fn calc_memo_state(weights: &[f32; 17], item: FSRSItem) -> (f32, f32) {
+    type Backend = NdArrayBackend<f32>;
+    let model = weights_to_modela(weights);
+    let (time_history, rating_history) = item.reviews.iter().map(|r| (r.delta_t, r.rating)).unzip();
+    let size = item.reviews.len();
+    let time_history =
+        Tensor::<Backend, 1>::from_data(Data::new(time_history, Shape { dims: [size] }).convert())
+            .unsqueeze()
+            .transpose();
+    let rating_history = Tensor::<Backend, 1>::from_data(
+        Data::new(rating_history, Shape { dims: [size] }).convert(),
+    )
+    .unsqueeze()
+    .transpose();
+    let (stability, difficulty) = model.forward(time_history, rating_history);
+    (stability.to_data().value[0], difficulty.to_data().value[0])
+}
+
+pub fn next_memo_state(
+    weights: &[f32; 17],
+    review: FSRSReview,
+    i: usize,
+    last_s: f32,
+    last_d: f32,
+) -> (f32, f32) {
+    type Backend = NdArrayBackend<f32>;
+    let model = weights_to_modela(weights);
+    let delta_t = Tensor::<Backend, 1>::from_data(Data::new(
+        vec![review.delta_t as f32],
+        Shape { dims: [1] },
+    ))
+    .unsqueeze()
+    .transpose();
+    let rating =
+        Tensor::<Backend, 1>::from_data(Data::new(vec![review.rating as f32], Shape { dims: [1] }))
+            .unsqueeze()
+            .transpose();
+    let stability = Tensor::<Backend, 1>::from_data(Data::new(vec![last_s], Shape { dims: [1] }))
+        .unsqueeze()
+        .transpose();
+    let difficulty = Tensor::<Backend, 1>::from_data(Data::new(vec![last_d], Shape { dims: [1] }))
+        .unsqueeze()
+        .transpose();
+    let (stability, difficulty) = model.step(i, delta_t, rating, stability, difficulty);
+    (stability.to_data().value[0], difficulty.to_data().value[0])
+}
+
+pub fn next_interval(stability: f32, request_retention: f32) -> i32 {
+    (9.0 * stability * (1.0 / request_retention - 1.0))
+        .round()
+        .max(1.0) as i32
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct ItemProgress {
+    pub current: usize,
+    pub total: usize,
+}
+
+pub fn evaluate<F>(weights: &[f32; 17], items: Vec<FSRSItem>, mut progress: F) -> Result<(f32, f32)>
+where
+    F: FnMut(ItemProgress) -> bool,
+{
+    type Backend = NdArrayBackend<f32>;
+    let device = NdArrayDevice::Cpu;
+    let batcher = FSRSBatcher::<Backend>::new(device);
+    let model = weights_to_modela(weights);
     let mut all_pred = vec![];
     let mut all_true_val = vec![];
     let mut all_retention = vec![];
@@ -115,7 +174,7 @@ fn calibration_rmse(pred: Vec<f32>, true_val: Vec<f32>) -> f32 {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::convertor_tests::anki21_sample_file_converted_to_fsrs;
+    use crate::{convertor_tests::anki21_sample_file_converted_to_fsrs, FSRSReview};
 
     #[test]
     fn test_get_bin() {
@@ -133,11 +192,79 @@ mod tests {
     }
 
     #[test]
+    fn test_memo_state() {
+        let weights = &[
+            0.81497127,
+            1.5411042,
+            4.007436,
+            9.045982,
+            4.9264183,
+            1.039322,
+            0.93803364,
+            0.0,
+            1.5530516,
+            0.10299722,
+            0.9981442,
+            2.210701,
+            0.018248068,
+            0.3422524,
+            1.3384504,
+            0.22278537,
+            2.6646678,
+        ];
+        let item = FSRSItem {
+            reviews: vec![
+                FSRSReview {
+                    rating: 1,
+                    delta_t: 0,
+                },
+                FSRSReview {
+                    rating: 3,
+                    delta_t: 1,
+                },
+                FSRSReview {
+                    rating: 3,
+                    delta_t: 3,
+                },
+                FSRSReview {
+                    rating: 3,
+                    delta_t: 8,
+                },
+                FSRSReview {
+                    rating: 3,
+                    delta_t: 21,
+                },
+            ],
+        };
+        assert_eq!(calc_memo_state(weights, item), (51.344814, 7.005062,));
+
+        let review = FSRSReview {
+            rating: 3,
+            delta_t: 21,
+        };
+
+        assert_eq!(
+            next_memo_state(weights, review, 5, 20.925528, 7.005062),
+            (51.344814, 7.005062)
+        );
+    }
+
+    #[test]
+    fn test_next_interval() {
+        let request_retentions = (1..=10).map(|i| i as f32 / 10.0).collect::<Vec<_>>();
+        let intervals = request_retentions
+            .iter()
+            .map(|r| next_interval(1.0, *r))
+            .collect::<Vec<_>>();
+        assert_eq!(intervals, [81, 36, 21, 14, 9, 6, 4, 2, 1, 1,]);
+    }
+
+    #[test]
     fn test_evaluate() {
         let items = anki21_sample_file_converted_to_fsrs();
 
         let metrics = evaluate(
-            [
+            &[
                 0.4, 0.6, 2.4, 5.8, 4.93, 0.94, 0.86, 0.01, 1.49, 0.14, 0.94, 2.18, 0.05, 0.34,
                 1.26, 0.29, 2.61,
             ],
@@ -150,7 +277,7 @@ mod tests {
             .assert_approx_eq(&Data::from([0.20820294, 0.042998276]), 5);
 
         let metrics = evaluate(
-            [
+            &[
                 0.81497127,
                 1.5411042,
                 4.007436,
