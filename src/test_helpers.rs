34570f396c86df857dd149921b9ed91c37993321
Damien Elmes <dae@users.noreply.github.com>
2023-12-07 13:18:58 +1000

    Dependency updates (#127)
    * Update to latest burn; handle API changes
    
    * Upgrade other deps

--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -1,5 +1,6 @@
-use burn::backend::NdArrayAutodiffBackend;
+use burn::backend::autodiff::Autodiff;
+pub type NdArrayAutodiff = Autodiff<burn::backend::NdArray>;
 use burn::tensor::Float;
 
-pub type Model = crate::model::Model<NdArrayAutodiffBackend>;
-pub type Tensor<const D: usize, K = Float> = burn::tensor::Tensor<NdArrayAutodiffBackend, D, K>;
+pub type Model = crate::model::Model<NdArrayAutodiff>;
+pub type Tensor<const D: usize, K = Float> = burn::tensor::Tensor<NdArrayAutodiff, D, K>;
