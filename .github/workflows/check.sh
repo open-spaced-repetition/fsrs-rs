e69d0ef5fb3597e541ea3761517d5a7a58f09306
Jarrett Ye <jarrett.ye@outlook.com>
2024-04-03 22:28:25 +0800

    skip test & use nightly

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -9,9 +9,3 @@ cargo fmt --check || (
 )
 
 cargo clippy -- -Dwarnings
-
-install -d tests/data/
-pushd tests/data/
-wget https://github.com/open-spaced-repetition/fsrs-optimizer-burn/files/12394182/collection.anki21.zip
-unzip *.zip
-SKIP_TRAINING=1 cargo test --release
