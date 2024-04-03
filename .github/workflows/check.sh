383aeb9c4d6581ac50d64fc1ceb2a06a025daefa
Alex <git@pentive.com>
2024-04-03 16:01:54 -0500

    Revert "skip test & use nightly"
    This reverts commit e69d0ef5fb3597e541ea3761517d5a7a58f09306.

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -9,3 +9,9 @@ cargo fmt --check || (
 )
 
 cargo clippy -- -Dwarnings
+
+install -d tests/data/
+pushd tests/data/
+wget https://github.com/open-spaced-repetition/fsrs-optimizer-burn/files/12394182/collection.anki21.zip
+unzip *.zip
+SKIP_TRAINING=1 cargo test --release
